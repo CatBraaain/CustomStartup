8c47d1967827b02817da88a3b9d82cec4a437980
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2022-12-24 22:57:30 +0900

    Update Startup.ps1

--- Startup.ps1
+++ Startup.ps1
@@ -1,9 +1,10 @@
 function Run($FilePath,$ArgList,$WinTitle){
-    $FolderPath = $(Split-Path -Path $FilePath -Parent)
+    #$FolderPath = $(Split-Path -Path $FilePath -Parent)
+    # -WorkingDirectory $FolderPath
     if($ArgList -eq ""){
-        Start-Process -FilePath $FilePath -WorkingDirectory $FolderPath
+        Start-Process -FilePath $FilePath
     }else{
-        Start-Process -FilePath $FilePath -WorkingDirectory $FolderPath -ArgumentList $ArgList
+        Start-Process -WindowStyle Hidden  -FilePath $FilePath -ArgumentList $ArgList
     }
     While((Get-Process|Where-Object {$_.MainWindowTitle -like $WinTitle}) -eq $null){
         SLEEP 0.5
@@ -11,12 +12,18 @@ function Run($FilePath,$ArgList,$WinTitle){
 }
 
 Run "C:\Program Files\AutoHotkey\Scripts\ScreenLock.ahk" "" "*"
+Run "C:\Program Files\Google\Drive File Stream\launch.bat" "" "*"
 Run "C:\Program Files\obs-studio\bin\64bit\obs64.exe" "--startreplaybuffer --minimize-to-tray" "*"
+Run "explorer.exe" "" "*"
+Run "Taskmgr.exe" "" "*タスク マネージャー*"
 Run "C:\Program Files (x86)\Notepad++\notepad++.exe" "" "* - Notepad++"
-Run "C:\Users\PRO\AppData\Local\Programs\Microsoft VS Code\Code.exe" "-n `"C:\Program` Files\AutoHotkey`"" "* - AutoHotkey - Visual Studio Code"
-Run "C:\Users\PRO\AppData\Local\Programs\Microsoft VS Code\Code.exe" "-n `"C:\Users\PRO\Documents\カスタマイズ\自作拡張機能`"" "* - 自作拡張機能 - Visual Studio Code"
+Run "code" "-n -g `"C:\Program` Files\AutoHotkey`"" "* - AutoHotkey - Visual Studio Code"
+Run "code" "-n -g `"C:\Users\PRO\Documents\カスタマイズ\自作拡張機能`"" "* - 自作拡張機能 - Visual Studio Code"
 Run "C:\Users\PRO\Documents\タスク管理.xlsm" "" "*タスク管理.xlsm*"
 Run "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "" "* - Google Chrome"
 
-#Get-Process|Where-Object {$_.MainWindowTitle}|Format-Table *
+#メモ
+#VSCodeには-WindowStyle Hiddenがないといつまでもプロンプトが表示される
+#excelには-WindowStyle Hiddenがあるとウィンドウが小さく表示される
+
 #Get-Process|Where-Object {$_.MainWindowTitle}|Format-Table MainWindowTitle
