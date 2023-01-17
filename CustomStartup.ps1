96141ac04d1d99d8b5df6e48ed3b22bfd9aba31b
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2023-01-17 20:43:15 +0900

    Add files via upload
    ①タスクスケジューラでCustomStartup.vbsは起動、管理者権限つきでCustomStartupRunas.vbs起動
    ②VBSからPowershellをウィンドウ非表示で起動
    ③アプリを順番に起動

---
+++ CustomStartup.ps1
@@ -0,0 +1,35 @@
+﻿
+
+function Run($FilePath,$ArgList,$WinTitle){
+    $FolderPath = $(Split-Path -Path $FilePath -Parent)
+    $Command = "Start-Process -FilePath ""$FilePath"""
+    if($FolderPath -ne ""){$Command = $Command + " -WorkingDirectory ""$FolderPath"""}
+    if($ArgList -ne $null){$Command = $Command + " -WindowStyle Hidden -ArgumentList ""$ArgList"""}
+    echo $Command
+    Invoke-Expression $Command
+
+    if($WinTitle -ne $null){
+        While((Get-Process|Where-Object {$_.MainWindowTitle -like $WinTitle}) -eq $null){
+            SLEEP 0.5
+        }
+    }
+}
+
+Run "C:\Program Files\AutoHotkey\Scripts\ScreenLock.ahk"
+Run "C:\Program Files (x86)\PicPick\picpick.exe"
+Run "C:\Program Files\obs-studio\bin\64bit\obs64.exe" -ArgList "--startreplaybuffer --minimize-to-tray"
+Run "explorer.exe"
+Run "C:\Program Files (x86)\Notepad++\notepad++.exe" -WinTitle "* - Notepad++"
+Run "Code" -ArgList "-n -g """"C:\Program Files\AutoHotkey""""" -WinTitle "* - AutoHotkey - Visual Studio Code"
+Run "Code" -ArgList "-n -g """"C:\Users\PRO\Documents\カスタマイズ\自作拡張機能""""" -WinTitle "* - 自作拡張機能 - Visual Studio Code"
+Run "C:\Users\PRO\Documents\タスク管理.xlsm" -WinTitle "*タスク管理.xlsm*"
+Run "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" -WinTitle "* - Google Chrome"
+Run "C:\Program Files\Google\Drive File Stream\launch.bat"
+exit
+
+#メモ
+# -WindowStyle Hidden 
+#VSCodeには-WindowStyle Hiddenがないといつまでもプロンプトが表示される
+#excelには-WindowStyle Hiddenがあるとウィンドウが小さく表示される
+
+#Get-Process|Where-Object {$_.MainWindowTitle}|Format-Table MainWindowTitle
---
+++ CustomStartupRunas.ps1
@@ -0,0 +1,36 @@
+﻿if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole("Administrators")) { Start-Process powershell.exe "-File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+function RunAs($FilePath,$ArgList,$WinTitle){
+    $FolderPath = $(Split-Path -Path $FilePath -Parent)
+    $Command = "Start-Process -FilePath ""$FilePath"""
+    if($FolderPath -ne ""){$Command = $Command + " -WorkingDirectory ""$FolderPath"""}
+    if($ArgList -ne $null){$Command = $Command + " -WindowStyle Hidden -ArgumentList ""$ArgList"""}
+    echo $Command
+    Invoke-Expression $Command
+    
+    if($WinTitle -ne $null){
+        While((Get-Process|Where-Object {$_.MainWindowTitle -like $WinTitle}) -eq $null){
+            SLEEP 0.5
+        }
+    }
+}
+function HideWin($WinTitle){
+    $Signature = @"
+    [DllImport("user32.dll")]public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
+"@
+
+    $ShowWindowAsync = Add-Type -MemberDefinition $Signature -Name "Win32ShowWindowAsync" -Namespace Win32Functions -PassThru
+    $ShowWindowAsync::ShowWindowAsync((Get-Process|Where-Object {$_.MainWindowTitle -like "*タスク マネージャー*"}).MainWindowHandle, 6)
+}
+
+RunAs "C:\Program Files\AutoHotkey\AutoHotkey.exe" """""C:\Program Files\AutoHotkey\AutoHotkey.ahk"""""
+RunAs "C:\Program Files (x86)\MSI Afterburner\MSIAfterburner.exe"
+RunAs "Taskmgr.exe" -WinTitle "タスク マネージャー"
+HideWin "タスク マネージャー"
+exit
+
+#メモ
+#VSCodeには-WindowStyle Hiddenがないといつまでもプロンプトが表示される
+#excelには-WindowStyle Hiddenがあるとウィンドウが小さく表示される
+
+#Get-Process|Where-Object {$_.MainWindowTitle}|Format-Table MainWindowTitle
